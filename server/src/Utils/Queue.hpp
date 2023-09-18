59a99d71540c5565f0cae5ec1eb80f1530827e9c
Mazettt <martin.d-herouville@epitech.eu>
2023-09-18 16:37:45 +0200

    (+) safe queue class

---
+++ server/src/Utils/Queue.hpp
@@ -0,0 +1,54 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+
+template<typename T>
+class Queue {
+    public:
+        Queue() = default;
+        ~Queue() = default;
+
+        void push(T &&value);
+        bool tryPop(T &value);
+        T pop();
+        std::size_t size() const { return _queue.size(); }
+
+    private:
+        std::mutex _mutex;
+        std::condition_variable _cv;
+        std::queue<T> _queue;
+};
+
+template<typename T>
+void Queue<T>::push(T &&value)
+{
+    std::unique_lock lock(_mutex);
+    _queue.push(std::move(value));
+    _cv.notify_one();
+}
+
+template<typename T>
+bool Queue<T>::tryPop(T &value)
+{
+    std::unique_lock lock(_mutex);
+    if (_queue.empty())
+        return false;
+    value = std::move(_queue.front());
+    _queue.pop();
+    return true;
+}
+
+template<typename T>
+T Queue<T>::pop()
+{
+    std::unique_lock lock(_mutex);
+    if (_queue.empty())
+        _cv.wait(lock);
+    T value = _queue.front();
+    _queue.pop();
+    return value;
+}
