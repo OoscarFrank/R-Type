fc430b47489f8d79f337ceae9d5d44ccca24ac3d
Mazettt <martin.d-herouville@epitech.eu>
2023-09-20 18:16:47 +0200

    (+) comments to document code in Utils/ folder

--- server/src/Utils/Args.hpp
+++ server/src/Utils/Args.hpp
@@ -5,6 +5,10 @@
 #include <algorithm>
 #include "MyError.hpp"
 
+/**
+ * @brief Class that can be used to parse command line arguments and get flags values
+ *
+ */
 class Args {
     public:
         class Error: public MyError {
@@ -20,22 +24,65 @@ class Args {
                 Info(const std::string &message, const std::string &className = "Args"): MyError(className, message, false, "INFO") {}
         };
 
+        /**
+         * @brief Construct a new Args object and parse the command line arguments
+         *
+         * @param ac Number of arguments
+         * @param av Arguments
+         */
         Args(int ac, char **av);
         Args(const Args &other) = delete;
         Args(Args &&other) = delete;
-        ~Args();
+        ~Args() = default;
 
         Args &operator=(const Args &other) = delete;
         Args &operator=(Args &&other) = delete;
 
+        /**
+         * @brief Check if a flag is entered in the command line arguments
+         *
+         * @param flag Flag to check
+         * @return true If the flag is set
+         * @return false If the flag is not set
+         */
         bool isFlagSet(const std::string &flag) const;
 
+        /**
+         * @brief Get the value of the flag
+         *
+         * @tparam T Type of the value
+         * @param flag Flag to get the value of
+         * @return T
+         */
         template<typename T>
         T getFlagValue(const std::string &flag) const;
+        /**
+         * @brief Get the value of the flag or a default value if the flag is not set
+         *
+         * @tparam T Type of the value
+         * @param flag Flag to get the value of
+         * @param defaultValue Default value if the flag is not set
+         * @return T
+         */
         template<typename T>
         T getFlagValue(const std::string &flag, const T &defaultValue) const;
+        /**
+         * @brief Get the values of the flag
+         *
+         * @tparam T Type of the values
+         * @param flag Flag to get the values of
+         * @return std::vector<T>
+         */
         template<typename T>
         std::vector<T> getFlagValues(const std::string &flag) const;
+        /**
+         * @brief Get the values of the flag or default values if the flag is not set
+         *
+         * @tparam T Type of the values
+         * @param flag Flag to get the values of
+         * @param defaultValues Default values if the flag is not set
+         * @return std::vector<T>
+         */
         template<typename T>
         std::vector<T> getFlagValues(const std::string &flag, const std::vector<T> &defaultValues) const;
 
