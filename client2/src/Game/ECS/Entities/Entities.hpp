46d4b83c156e82eb7b94ffd3904f1a16c4516587
LucasB9 <lucas.binder@epitech.eu>
2023-09-28 13:08:44 +0200

    [-] remove client2

--- client2/src/Game/ECS/Entities/Entities.hpp
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include "./Player.hpp"
-#include "./Parallax.hpp"
-#include "./Missile.hpp"
--- client2/src/Utils.hpp
+++
@@ -1,3 +0,0 @@
-#pragma once
-
-#define NOW std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
