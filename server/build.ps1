42f687869ca4366792932144b1b5de3c1053c5fd
Mazettt <martin.d-herouville@epitech.eu>
2023-10-03 18:23:00 +0200

    (|) fix wrong make in bash build script

--- server/build.ps1
+++ server/build.ps1
@@ -2,6 +2,6 @@ Remove-Item -Path "build" -Recurse -Force
 New-Item -ItemType Directory -Name "build"
 Set-Location "build"
 Invoke-Expression "cmake .."
+Invoke-Expression "msbuild .\r-type_server.vcxproj /p:Configuration=Release"
+Invoke-Expression "msbuild .\PACKAGE.vcxproj /p:Configuration=Release"
 Set-Location ".."
-Invoke-Expression "msbuild .\build\r-type_server.vcxproj /p:Configuration=Release"
-Invoke-Expression "msbuild .\build\PACKAGE.vcxproj /p:Configuration=Release"
--- server/build.sh
+++ server/build.sh
@@ -2,6 +2,6 @@ rm -rf build/
 mkdir build
 cd build
 cmake ..
-cd ..
 make
 make package
+cd ..
