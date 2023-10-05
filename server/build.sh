15d27e6bf6809368a3662219cc287a17eff26e25
Mazettt <76044580+Mazettt@users.noreply.github.com>
2023-10-05 09:41:01 +0200

    (+) gitHub actions: build and create release on tag / check compilation on PR
    * (+) test compilation
    
    * (+) test compilation
    
    * (+) install make and cmake
    
    * (+) install make and cmake
    
    * (|) install make and cmake
    
    * (|) install rpmbuild (ubuntu) and msbuild (windows)
    
    * (+) install submodules windows
    
    * (+) build working: test commands to check files
    
    * (+) upload artifacts of build files
    
    * (+) download artifacts and test
    
    * (+) rename downloaded artifacts with good version
    
    * (+) create release with artifacts
    
    * (|) create release with artifacts
    
    * (|) run action on new tag
    
    * (|) remove warning caused by actions/create_release: now using gh cli
    
    * (|) test
    
    * (+) install gh before creating release
    
    * (+) gh action to check compilation on pull requests

--- server/build.sh
+++ server/build.sh
@@ -1,7 +1,21 @@
+git submodule update --init --recursive
+
 rm -rf build/
 mkdir build
 cd build
 cmake ..
+if [ $? -ne 0 ]; then
+    echo cmake failed
+    exit 1
+fi
 make
+if [ $? -ne 0 ]; then
+    echo make failed
+    exit 1
+fi
 make package
+if [ $? -ne 0 ]; then
+    echo make package failed
+    exit 1
+fi
 cd ..
