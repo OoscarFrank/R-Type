3b01a9c331d7294dbdfcdfb76a8cff39d9d1af7f
Mazettt <martin.d-herouville@epitech.eu>
2023-10-15 21:15:44 +0200

    (+) install libxcursor in build.sh

--- client/build.sh
+++ client/build.sh
@@ -5,10 +5,10 @@ git submodule update --init --recursive
 source /etc/os-release
 
 if [ "$ID" == "ubuntu" ]; then
-    sudo apt install -y libfreetype6 x11-xserver-utils libflac-dev libogg-dev libvorbis-dev libopenal-dev libsfml-dev libx11-dev libxrandr-dev udev libudev-dev libfreetype6-dev
+    sudo apt install -y libfreetype6 x11-xserver-utils libflac-dev libogg-dev libvorbis-dev libopenal-dev libsfml-dev libx11-dev libxcursor-dev libxrandr-dev udev libudev-dev libfreetype6-dev
     echo "Dependencies on Ubuntu for SFML installed"
 elif [ "$ID" == "fedora" ]; then
-    sudo dnf install -y freetype-devel libX11-devel libXrandr-devel libudev-devel xorg-x11-server-Xorg mesa-libGL-devel flac-devel libogg-devel libvorbis-devel openal-soft-devel
+    sudo dnf install -y freetype-devel libX11-devel libXcursor-devel libXrandr-devel libudev-devel xorg-x11-server-Xorg mesa-libGL-devel flac-devel libogg-devel libvorbis-devel openal-soft-devel
     echo "Dependencies on Fedora for SFML installed"
 else
     echo "This is a different Linux distribution"
